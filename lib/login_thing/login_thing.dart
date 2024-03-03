a65ceda7da395ad35bc83cf39d0f3e5ddeb48d8a
noahstelmak <noah.bussmann@gmail.com>
2024-03-02 21:15:32 -0300

    Textfield conectado ao teclado virtual

--- lib/login_thing/login_thing.dart
+++ lib/login_thing/login_thing.dart
@@ -1,7 +1,4 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:virtual_secure_keyboard/virtual_keyboard/virtual_keyboard.dart';
 import 'package:virtual_secure_keyboard/virtual_keyboard/virtual_keys.dart';
@@ -15,6 +12,7 @@ class LoginThing extends StatefulWidget {
 
 class _LoginThingState extends State<LoginThing> {
   final passwordField = FocusNode();
+  final controller = TextEditingController();
 
   @override
   void initState() {
@@ -48,6 +46,7 @@ class _LoginThingState extends State<LoginThing> {
                 obscureText: true,
                 focusNode: passwordField,
                 readOnly: true,
+                controller: controller,
                 textInputAction: TextInputAction.done,
                 decoration: InputDecoration(
                   icon: const Icon(Icons.lock),
@@ -71,35 +70,33 @@ class _LoginThingState extends State<LoginThing> {
                     "8 ou 9",
                   ]),
                   onKeyPressed: (value) {
-                    /*  String text = controller.text;
-                        TextSelection textSelection = controller.selection;
-              
-                        String newText = text.replaceRange(
-                            textSelection.start,
-                            textSelection.end,
-                            value.toString());
-                        controller.text = newText;
-                        controller.selection = textSelection.copyWith(
-                          baseOffset: textSelection.start + 1,
-                          extentOffset: textSelection.start + 1,
-                        ); */
+                    String text = controller.text;
+                    TextSelection textSelection = controller.selection;
+
+                    String newText = text.replaceRange(textSelection.start,
+                        textSelection.end, value.toString());
+                    controller.text = newText;
+                    controller.selection = textSelection.copyWith(
+                      baseOffset: textSelection.start + 1,
+                      extentOffset: textSelection.start + 1,
+                    );
                   },
                   onBackspacePressed: () {
-                    /*  String text = controller.text;
-                        final textSelection = controller.selection;
-                        if (textSelection.baseOffset > 0) {
-                          String newText = text.replaceRange(
-                            textSelection.start - 1,
-                            textSelection.end,
-                            "",
-                          );
-              
-                          controller.text = newText;
-                          controller.selection = textSelection.copyWith(
-                            baseOffset: textSelection.start - 1,
-                            extentOffset: textSelection.start - 1,
-                          );
-                        } */
+                    String text = controller.text;
+                    final textSelection = controller.selection;
+                    if (textSelection.baseOffset > 0) {
+                      String newText = text.replaceRange(
+                        textSelection.start - 1,
+                        textSelection.end,
+                        "",
+                      );
+
+                      controller.text = newText;
+                      controller.selection = textSelection.copyWith(
+                        baseOffset: textSelection.start - 1,
+                        extentOffset: textSelection.start - 1,
+                      );
+                    }
                   },
                 ),
               ),
